220825802f4e2fdde6250016ad2dcfa5fa8d8277
Unknown <paul.thebaud29@gmail.com>
2018-02-14 15:22:28 +0100

    Remove HTTPS

--- public/index.php
+++ public/index.php
@@ -12,13 +12,6 @@ if (PHP_SAPI == 'cli-server') {
     }
 }
 
-// Redirect when accessing over HTTP
-if(! isset($_SERVER['HTTPS']))
-{
-    header('Location: https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
-    exit();
-}
-
 require __DIR__ . '/../vendor/autoload.php';
 
 session_start();
