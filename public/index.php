268e39e710b91dcd8f9af4944c4c7b1d72bd5ba1
Unknown <paul.thebaud29@gmail.com>
2018-02-14 15:13:37 +0100

    Update to force HTTPS

--- public/index.php
+++ public/index.php
@@ -12,6 +12,13 @@ if (PHP_SAPI == 'cli-server') {
     }
 }
 
+// Redirect when accessing over HTTP
+if(! isset($_SERVER['HTTPS']) || $_SERVER['HTTPS'] != 'on')
+{
+    header('Location: https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
+    exit();
+}
+
 require __DIR__ . '/../vendor/autoload.php';
 
 session_start();
