41bc02526c44342ce5a42a8671ca7c41896a7352
Unknown <paul.thebaud29@gmail.com>
2018-03-09 18:49:33 +0100

    Remove HTTPS

--- src/middleware.php
+++ src/middleware.php
@@ -1,15 +1,2 @@
 <?php
 // Application middleware
-
-use Slim\Http\Request;
-use Slim\Http\Response;
-
-// Force HTTPS
-$app->add(function (Request $request, Response $response, $next) {
-    if ($request->getHeaderLine('X-Forwarded-Proto') !== 'https') {
-        $uri = $request->getUri()->withScheme("https")->withPort(null);
-        return $response->withRedirect((string)$uri);
-    } else {
-        return $next($request, $response);
-    }
-});
