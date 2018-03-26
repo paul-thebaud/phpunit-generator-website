05b83aedca19f35e72fe7d0fcd4d55c2f7e15779
Unknown <paul.thebaud29@gmail.com>
2018-03-26 19:57:53 +0200

    Add article

--- src/routes.php
+++ src/routes.php
@@ -21,3 +21,8 @@ $app->get('/documentation', function (Request $request, Response $response) {
 $app->post('/generate', function (Request $request, Response $response) {
     return $this->get(GenerateController::class)->generateAction($request, $response);
 });
+
+// FR version of the article about PhpUnitGen
+$app->post('/generate', function (Request $request, Response $response) {
+    return $this->get(PhpRenderer::class)->render($response, 'fr-article.html');
+});
