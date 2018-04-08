e39e85cba6004971d37d2b44f14a285b9aa2779e
Unknown <paul.thebaud29@gmail.com>
2018-04-08 03:07:39 +0200

    Reduce app loader useless complexity

--- public/assets/js/app.loader.js
+++ public/assets/js/app.loader.js
@@ -1,88 +1,14 @@
-/**
- * Represents the application.
- */
-var PhpUnitGen = {};
-
-/**
- * Represents the App Loader, a module to load app scripts.
- */
-PhpUnitGen.AppLoader = (function() {
-  var self = {};
-
-  /**
-   * @type {string[]} scripts A scripts to load array.
-   */
-  var scripts = [
-    'assets/js/lib/app.toast.js',
-    'assets/js/lib/app.theme.js',
-    'assets/js/lib/app.spinner.js',
-    'assets/js/lib/app.util.js',
-    'assets/js/lib/app.page.loader.js',
-    'assets/js/lib/app.form.loader.js',
-    'assets/js/lib/app.config.js',
-    'assets/js/lib/app.mobile.js',
-    'assets/js/lib/app.form.listener.js',
-    'assets/js/lib/app.request.js',
-    'assets/js/lib/app.tab.js',
-    'assets/js/lib/app.editor.listener.js'
-  ];
-
-  var currentIndex = 0;
-  var currentScript = scripts[0];
-  var finalCallback;
-
-  /**
-   * Initialize the module.
-   * @param finalCallbackFunction The callback function when init is finish.
-   */
-  self.initialize = function(finalCallbackFunction) {
-    finalCallback = finalCallbackFunction;
-    load();
-  };
-
-  /**
-   * Load a script.
-   */
-  function load() {
-    // Add script on page
-    var head = document.getElementsByTagName('body')[0];
-    var script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.src = currentScript;
-
-    var callback = function() {
-      // On script loaded
-      currentIndex = currentIndex + 1;
-      if (currentIndex in scripts) {
-        currentScript = scripts[currentIndex];
-        load();
-      } else {
-        finalCallback();
-      }
-    };
-
-    script.onreadystatechange = callback;
-    script.onload = callback;
-
-    // Fire the loading
-    head.appendChild(script);
-  }
-
-  return self;
-})();
-
 // On page is ready
-$(document).ready(function() {
-  // Load PhpUnitGen app
-  PhpUnitGen.AppLoader.initialize(function() {
-    // And initialize each modules
-    PhpUnitGen.Mobile.initialize();
-    PhpUnitGen.FormListener.initialize();
-    PhpUnitGen.PageLoader.initialize();
-    PhpUnitGen.Config.initialize();
-    PhpUnitGen.Tab.initialize();
-    PhpUnitGen.EditorListener.initialize();
+$(window).load(function() {
+  console.log('Its loaded');
 
-    $('#app-loader').fadeOut(500);
-  });
+  // Initialize each modules
+  PhpUnitGen.Mobile.initialize();
+  PhpUnitGen.FormListener.initialize();
+  PhpUnitGen.PageLoader.initialize();
+  PhpUnitGen.Config.initialize();
+  PhpUnitGen.Tab.initialize();
+  PhpUnitGen.EditorListener.initialize();
+
+  $('#app-loader').fadeOut(500);
 });
